c82346a57c2783c2328e713d5cc689ac534aae30
ghdaud30 <54072339+ghdaud30@users.noreply.github.com>
2023-10-05 16:56:18 +0900

    모델2방식의 자료실형 게시판

---
+++ MustHaveJSP/src/main/webapp/model2/mvcboard/MVCBoardDAO.java
@@ -0,0 +1,111 @@
+package model2.mvcboard;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import common.JDBConnect;
+
+public class MVCBoardDAO extends JDBConnect {
+
+    // 검색 조건에 맞는 게시물의 개수를 반환합니다.
+    public int selectCount(Map<String, Object> map) {
+        int totalCount = 0;
+        String query = "SELECT COUNT(*) FROM mvcboard";
+        if (map != null && map.get("searchWord") != null) {
+            query += " WHERE " + map.get("searchField") + " "
+                   + " LIKE '%" + map.get("searchWord") + "%'";
+        }
+        try {
+        	Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            totalCount = rs.getInt(1);
+        }
+        catch (Exception e) {
+            System.out.println("게시물 카운트 중 예외 발생");
+            System.out.println(e.getMessage());
+        }
+
+        return totalCount;
+    }
+
+    // 검색 조건에 맞는 게시물 목록을 반환합니다(페이징 기능 지원).
+    public List<MVCBoardDTO> selectListPage(Map<String,Object> map) {
+        List<MVCBoardDTO> board = new Vector<>();
+        String query = " "
+                     + "SELECT * FROM mvcboard ";
+
+        if (map != null && map.get("searchWord") != null)
+        {
+            query += " WHERE " + map.get("searchField")
+                   + " LIKE '%" + map.get("searchWord") + "%' ";
+        }
+
+        query += " ORDER BY idx DESC limit ? , ?";
+
+
+        try {
+        	Connection con = getConnection();
+        	PreparedStatement psmt = con.prepareStatement(query);
+            psmt.setInt(1, (int) map.get("start"));
+            psmt.setInt(2, (int) map.get("pagesize"));
+            ResultSet rs = psmt.executeQuery();
+
+            while (rs.next()) {
+                MVCBoardDTO dto = new MVCBoardDTO();
+
+                dto.setIdx(rs.getString(1));
+                dto.setName(rs.getString(2));
+                dto.setTitle(rs.getString(3));
+                dto.setContent(rs.getString(4));
+                dto.setPostdate(rs.getDate(5));
+                dto.setOfile(rs.getString(6));
+                dto.setSfile(rs.getString(7));
+                dto.setDowncount(rs.getInt(8));
+                dto.setPass(rs.getString(9));
+                dto.setVisitcount(rs.getInt(10));
+
+                board.add(dto);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("게시물 조회 중 예외 발생");
+            System.out.println(e.getMessage());
+        }
+        return board;
+    }
+    
+    // 게시글 데이터를 받아 DB에 추가합니다(파일 업로드 지원).
+    public int insertWrite(MVCBoardDTO dto) {
+        int result = 0;
+        try {
+            String query = "INSERT INTO mvcboard ( "
+                         + " name, title, content, ofile, sfile, pass) "
+                         + " VALUES ( "
+                         + " ?,?,?,?,?,?)";
+            Connection con = getConnection();
+            
+            PreparedStatement psmt = con.prepareStatement(query);
+            psmt.setString(1, dto.getName());
+            psmt.setString(2, dto.getTitle());
+            psmt.setString(3, dto.getContent());
+            psmt.setString(4, dto.getOfile());
+            psmt.setString(5, dto.getSfile());
+            psmt.setString(6, dto.getPass());
+            result = psmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println("게시물 입력 중 예외 발생");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+   
+}
